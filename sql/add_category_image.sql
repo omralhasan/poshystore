2da68520035779c34c43667b3afa6634181dd899
omralhasan <mate7762s@gmail.com>
2026-03-21 16:25:50 +0300

    some change on home page

--- sql/add_category_image.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Add image_path to categories and subcategories
--- Required for the new visual category stories and shop headers
-
-ALTER TABLE categories 
-ADD COLUMN IF NOT EXISTS image_path VARCHAR(500) DEFAULT NULL;
-
-ALTER TABLE subcategories 
-ADD COLUMN IF NOT EXISTS image_path VARCHAR(500) DEFAULT NULL;
